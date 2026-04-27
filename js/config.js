1476a1da355b1ed8a16aba608d46128b6e4ef182
NEXUS-Lord <nilamx3000@gmail.com>
2026-04-27 18:00:29 +0530

    add config.js

---
+++ js/config.js
@@ -0,0 +1,26 @@
+window.CONFIG = {
+    GAME_DURATION: 180,
+    CITY_SIZE: 200,
+    BUILDING_COUNT: 80,
+    ROAD_WIDTH: 6,
+    PLAYER_SPEED: 0.3,
+    PLAYER_TURN_SPEED: 0.03,
+    WATER_RISE_RATE: 0.008,
+    WATER_START_Y: -5,
+    WATER_MAX_Y: 8,
+    DELIVERY_COUNT: 6,
+    COLORS: {
+        building: 0xE8D5B0,
+        road: 0x4A4A4A,
+        tree: 0x2D5A27,
+        water: 0x1A6B8A,
+        player: 0xFF4500,
+        marker: 0xFFD700,
+        sky: 0x87CEEB,
+        fog: 0xB0C4DE
+    },
+    AUDIO: {
+        musicVolume: 0.4,
+        sfxVolume: 0.8
+    }
+};
